59f93b818a80e7021a47c49888a68e75a2c50b77
hoangjam <36323359+hoangjam@users.noreply.github.com>
2023-05-04 19:11:17 +0000

    formatted, added comments

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ alphabet = [
     't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
+
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
+    # check if the chars exist in the alphabet, if not just leave it as is
     for char in start_text:
         if char in alphabet:
             position = alphabet.index(char)
@@ -21,6 +23,7 @@ def caesar(start_text, shift_amount, cipher_direction):
 
     print(f"Here's the {cipher_direction}d result: {end_text}")
 
+
 print(logo)
 
 exit = False
@@ -30,9 +33,10 @@ while exit is False:
     shift = int(input("Type the shift number:\n"))
     if shift > 26:
         shift %= 26
-        
+
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-    choice = input("If you want to encode more type \'yes\'. Otherwise type 'no'")
+    choice = input(
+        "If you want to encode more type \'yes\'. Otherwise type 'no'")
     if choice == "no":
         exit = True
         print("Goodbye.")
